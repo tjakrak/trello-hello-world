f7d357c6c935b7b75b0f82dbcd8171c932fd9a98
ryantjakrakartadinata <rgtjakrakartadinata@dons.usfca.edu>
2022-09-17 03:53:53 -0700

    added button

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,3 +1,9 @@
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
 
 window.TrelloPowerUp.initialize({
     'board-buttons': function (t, opts) {
@@ -10,17 +16,6 @@ window.TrelloPowerUp.initialize({
             text: 'Callback',
             callback: onBtnClick,
             condition: 'edit'
-        }, {
-            // or we can also have a button that is just a simple url
-            // clicking it will open a new tab at the provided url
-            icon: {
-            dark: WHITE_ICON,
-            light: BLACK_ICON
-            },
-            text: 'URL',
-            condition: 'always',
-            url: 'https://trello.com/inspiration',
-            target: 'Inspiring Boards' // optional target for above url
-        }];
+        }]
     }
 })
